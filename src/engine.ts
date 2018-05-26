67e782ff73bb5934bbfe6b51f6cd83465795f816
amir-arad <greenshade@gmail.com>
2018-05-27 00:53:04 +0300

    change class style for engine improve typings init thre for built in plugins use three's built in orbit control plugin

--- src/engine.ts
+++ src/engine.ts
@@ -1,44 +1,52 @@
-'use strict'
+'use strict';
 
-const THREE = require('three')
-const OrbitControls = require('three-orbit-controls')(THREE)
+// loader for three.js examples, see https://github.com/mrdoob/three.js/issues/9562
+import * as _THREE from 'three';
+declare global {
+    interface Window {
+        THREE: typeof _THREE;
+    }
+}
+window.THREE = _THREE;
+//load three.js example extentions
+import 'three/examples/js/controls/OrbitControls.js';
+
+import {BoxGeometry, Mesh, MeshNormalMaterial, PerspectiveCamera, Scene, WebGLRenderer, OrbitControls} from 'three';
 
 export class Engine {
-  private three: any
-  renderer: any
-  scene: any
-  camera: any
-  controls: any
-  constructor () {
-    this.renderer = new THREE.WebGLRenderer({antialias: true})
-    this.scene = new THREE.Scene()
-    this.camera = new THREE.PerspectiveCamera(
-      70,
-      window.innerWidth / window.innerHeight,
-      0.01,
-      1000
-    )
-    this.controls = new OrbitControls(this.camera)
-    this.renderer.setSize(window.innerWidth, window.innerHeight)
-    this.animate()
-    document.body.appendChild(this.renderer.domElement)
-    window.addEventListener('resize', () => { // TODO: betterify
-      this.renderer.setSize(window.innerWidth, window.innerHeight)
-      this.camera.aspect = window.innerWidth / window.innerHeight
-      this.camera.updateProjectionMatrix()		
-    }, false)
-  }
-  addCube (x: number, y: number, z: number) {
-    const geometry = new THREE.BoxGeometry(50, 50, 50)
-    const material = new THREE.MeshNormalMaterial()
-    const cube = new THREE.Mesh( geometry, material )
-    cube.position.x = x
-    cube.position.y = y
-    cube.position.z = z
-    this.scene.add(cube)
-  }
-  private animate () {
-    this.renderer.render(this.scene, this.camera);		
-    requestAnimationFrame(() => this.animate()) // TODO: let's do this better
-  }
+    renderer = new WebGLRenderer({antialias: true});
+    scene = new Scene();
+    camera = new PerspectiveCamera(
+        70,
+        window.innerWidth / window.innerHeight,
+        0.01,
+        1000
+    );
+    controls = new OrbitControls(this.camera);
+
+    constructor() {
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.animate();
+        document.body.appendChild(this.renderer.domElement);
+        window.addEventListener('resize', () => { // TODO: betterify
+            this.renderer.setSize(window.innerWidth, window.innerHeight);
+            this.camera.aspect = window.innerWidth / window.innerHeight;
+            this.camera.updateProjectionMatrix()
+        }, false)
+    }
+
+    addCube(x: number, y: number, z: number) {
+        const geometry = new BoxGeometry(50, 50, 50);
+        const material = new MeshNormalMaterial();
+        const cube = new Mesh(geometry, material);
+        cube.position.x = x;
+        cube.position.y = y;
+        cube.position.z = z;
+        this.scene.add(cube)
+    }
+
+    private animate() {
+        this.renderer.render(this.scene, this.camera);
+        requestAnimationFrame(() => this.animate()) // TODO: let's do this better
+    }
 }
